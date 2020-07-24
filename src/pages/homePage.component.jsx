5b6dc9dfc035c17d3e2fb7de24ac44dfa6611270
jay-web <jay.developer12@gmail.com>
2020-07-24 20:26:01 +0530

    Homepage added and broken into directory and menu item component, also added css for these component

---
+++ src/pages/homePage.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './homePage.styles.scss';
+import Directory from '../components/directory/directory.component';
+
+const HomePage = () => (
+
+            <div className="homePage">
+                <Directory />
+            </div>
+        );
+
+
+export default HomePage;
