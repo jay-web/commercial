188a8678d4e1e35bc253ff9bbdcd0fabb66b20a2
jay-web <jay.developer12@gmail.com>
2020-07-24 21:02:20 +0530

    React router dom package added for routing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import {Switch, Route } from 'react-router-dom';
+
 import HomePage from './pages/homePage.component';
 
 
@@ -6,7 +8,19 @@ import HomePage from './pages/homePage.component';
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+      <Route exact path="/" component={HomePage}  />
+      <Route exact path="/121" component={PageNotFound} />
+      </Switch>
+    </div>
+  );
+}
+
+function PageNotFound() {
+  return (
+    <div>
+      <h1>Page not found</h1>
+      <h2>404</h2>
     </div>
   );
 }
