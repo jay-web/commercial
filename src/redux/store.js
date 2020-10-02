c52a7545abf18ab580ee792e2349fe3f265930f9
apple <apple@apples-MacBook-Pro-2.local>
2020-10-02 12:15:25 +0530

    Added logger middleware only in development mode

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,7 +3,11 @@ import combineReducers from "./root-reducer";
 import logger from "redux-logger";
 import { persistStore } from "redux-persist";
 
-const middleware = [logger];
+const middleware = [];
+
+if(process.env.NODE_ENV === "development"){
+    middleware.push(logger);
+}
 
 
 export const store = createStore(combineReducers, applyMiddleware(...middleware));
